73eb2721c0d36af2d43e5393b822749152f93f53
Daniel <codingwdan@gmail.com>
2024-03-13 20:49:32 -0700

    Updated the config files

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 //load config
-dotenv.config({ path: './config/config.env' })
+require("dotenv").config({ path: './config/config.env' })
 
 //Passport config
 require('./config/passport')(passport)
